54f66d6d29b321ddcff32a74e174445a59f36715
julien <j.paput@gmail.com>
2020-03-05 15:06:43 +0100

    Add JoinRoom Send SupervisorMessage to Room

--- core/app.js
+++ core/app.js
@@ -2,13 +2,11 @@
 
 const express = require('express');
 const socketIO = require('socket.io');
-const path = require('path');
 const cors = require('cors');
 const router = require('./router');
 
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, '../web/index.html');
 
 const server = express()
     .use(express.json())
@@ -22,39 +20,59 @@ const io = socketIO(server);
 
 const SERVER_MESSAGE = "serverMessage"
 const SUPERVISOR_MESSAGE = "supervisorMessage"
+
+const CONNECTION = 'connection'
 const USER_JOIN = "join"
+const USER_JOIN_ROOM = "joinRoom"
+
 const USER_LEAVE = "disconnect"
 
 const SERVER = "server"
 const SUPERVISOR = "supervisor"
 
-io.on('connection', (socket) => {
+
+io.on(CONNECTION, (socket) => {
 
     socket.on(USER_LEAVE, () => {
         console.log('Client disconnected')
     });
 
-    socket.on(USER_JOIN, userNickname => {
-
-        let message = userNickname +" est connecté"
-        console.log(message)
-
-        socket.broadcast.emit(
-            SERVER_MESSAGE,
-            {message : message, emitter : SERVER, date : makeDateString()});
+    socket.on(USER_JOIN_ROOM, room => {
+        socket.join(room);
+        let message = "user est connecté à " + room
+        broadcast(socket, message)
     });
 
-    socket.on(SUPERVISOR_MESSAGE, msg => {
-        console.log('Message from supervisor : ' + msg);
-        io.emit(
-            SUPERVISOR_MESSAGE,
-            {message : msg, emitter : SUPERVISOR, date : makeDateString()});
+    socket.on(USER_JOIN, userName => {
+        let message = userName +" est connecté"
+        broadcast(socket, message);
+    });
+
+    socket.on(SUPERVISOR_MESSAGE, payload => {
+        console.log('payload : ' +payload);
+        let jsonObject = JSON.parse(payload);
+        sendMessageToRoom(socket, jsonObject.message, jsonObject.room);
     });
 });
 
 function makeDateString(){
     let dateObj = new Date()
-    return dateObj.getUTCHours() +':'+ dateObj.getUTCMinutes() + ':' + dateObj.getSeconds();
+    return dateObj.getHours() +'h'+ dateObj.getMinutes();
+}
+
+
+function broadcast(socket, message) {
+    console.log('Broadcast message : ' +message);
+    socket.broadcast.emit(
+        SERVER_MESSAGE,
+        '{"message":"' + message +'","emitter":"' + SERVER +'","date":"'+makeDateString() +'"}');
+}
+
+function sendMessageToRoom(socket, message, room) {
+    console.log('Message from supervisor : ' + message + ' to room ' + room);
+    socket.in(room).emit(
+        SUPERVISOR_MESSAGE,
+        '{"message":"' + message +'","emitter":"' + SUPERVISOR +'","date":"'+makeDateString() +'"}');
 }
 
 setInterval(() => {
--- core/router.js
+++ core/router.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
+const INDEX = path.join(__dirname, '../web/index.html');
 const technicalApiV1 = require('../api/v1/technicalAPI');
 
-//Api
+//Api Technical
 router.use('/api/v1/technical', technicalApiV1);
+
+//web
+router.get('/', function(req, res) {
+    res.sendFile(INDEX);
+});
 
 
 
